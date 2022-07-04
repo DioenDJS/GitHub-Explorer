9a1c8d663ab6ddc6044aa7fcb7037bd770c1d377
Diovane Pacheti <negociospacheti@gmail.com>
2022-07-03 21:40:31 -0300

    :art: create first component

---
+++ src/components/RepositoryList/index.jsx
@@ -0,0 +1,15 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong></strong>
+                    <p></p>
+                    <a href=""></a>
+                </li>
+            </ul>
+        </section>
+    )
+}
